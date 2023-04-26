1bfa7934673d1c2d9f5ea967ab090d1b5cdb6e11
Adrian Fish <adrian.r.fish@gmail.com>
2023-04-26 11:03:16 +0100

    moved theme switcher js into library portal area

---
+++ library/src/webapp/js/portal/portal.theme.switcher.js
@@ -0,0 +1,78 @@
+var portal = portal || {};
+
+portal.darkThemeSwitcher = document.getElementById("sakai-dark-theme-switcher");
+
+portal.defaultThemeClass = 'sakaiUserTheme-notSet';
+portal.lightThemeClass = 'sakaiUserTheme-light';
+portal.darkThemeClass = 'sakaiUserTheme-dark';
+
+portal.toggleDarkTheme = () => {
+  // toggle the dark theme switch to the opposite state
+  portal.darkThemeSwitcher && portal.darkThemeSwitcher.getAttribute("aria-checked") === "false" ? portal.enableDarkTheme() : portal.enableLightTheme();
+};
+
+// if the dark theme switch is on the page, attach listener to dark theme toggle switch
+portal.darkThemeSwitcher && portal.darkThemeSwitcher.addEventListener('click', portal.toggleDarkTheme, false);
+
+if (portal.userThemeAutoDetectDark) {
+  if (portal.user.id) {
+    // only check for unset theme preference because light and dark themes are already set by Java
+    if (portal.userTheme === portal.defaultThemeClass) {
+        // if the user has dark mode set on their OS, enable dark mode
+        if (portal.isOsDarkThemeSet()) {
+            portal.enableDarkTheme();
+        } else {
+            // to define a user preference:
+            portal.setPortalThemeUserPref(portal.lightThemeClass);
+        }
+    }
+  } else if (portal.isOsDarkThemeSet()) {
+      // just add the dark theme to the markup if not logged in and the user has dark mode set on their OS (no prefs to save)
+      portal.addCssClassToMarkup(portal.darkThemeClass);
+  }
+}
+if (document.documentElement.classList.contains(portal.darkThemeClass)) {
+    // the dark theme switch toggle is off by default, so toggle it to on if dark theme is enabled
+    portal.setDarkThemeSwitcherToggle(true);
+}
+
+portal.addCssClassToMarkup = themeClass => {
+  document.documentElement.classList.add(themeClass);
+};
+
+portal.removeCssClassFromMarkup = themeClass => {
+  document.documentElement.classList.remove(themeClass);
+};
+
+portal.isOsDarkThemeSet = () => window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+portal.setDarkThemeSwitcherToggle = onOff => {
+  portal.darkThemeSwitcher && portal.darkThemeSwitcher.setAttribute("aria-checked", onOff);
+};
+
+portal.setPortalThemeUserPref = theme => {
+
+  var url = '/direct/userPrefs/updateKey/' + portal.user.id + '/sakai:portal:theme?theme=' + theme;
+  var ajaxRequest = new XMLHttpRequest();
+  ajaxRequest.open("PUT", url);
+  ajaxRequest.send();
+};
+
+
+portal.enableDarkTheme = () => {
+
+  portal.setDarkThemeSwitcherToggle(true);
+  portal.removeCssClassFromMarkup(portal.defaultThemeClass);
+  portal.removeCssClassFromMarkup(portal.lightThemeClass);
+  portal.addCssClassToMarkup(portal.darkThemeClass);
+  portal.setPortalThemeUserPref(portal.darkThemeClass);
+};
+
+portal.enableLightTheme = () => {
+
+  portal.setDarkThemeSwitcherToggle(false);
+  portal.removeCssClassFromMarkup(portal.defaultThemeClass);
+  portal.removeCssClassFromMarkup(portal.darkThemeClass);
+  portal.addCssClassToMarkup(portal.lightThemeClass);
+  portal.setPortalThemeUserPref(portal.lightThemeClass);
+};
