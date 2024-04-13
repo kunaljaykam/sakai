0e9cde97c5029bebac0f2f2d673cb6298308913a
kunaljaykam <kunaljaykam@gmail.com>
2024-04-13 14:44:08 +0530

    Add Tabulator

--- gradebookng/tool/src/webapp/scripts/gradebook-gbgrade-table.js
+++ gradebookng/tool/src/webapp/scripts/gradebook-gbgrade-table.js
@@ -756,6 +756,40 @@ GbGradeTable.renderTable = function (elementId, tableData) {
     $(this.TEXTAREA_PARENT).append(outOf);
   };
 
+  // Testing Tablutar
+
+  //define some sample data
+  var tabultorTestData = [
+    {id:1, name:"Oli Bob", age:"12", col:"red", dob:""},
+    {id:2, name:"Mary May", age:"1", col:"blue", dob:"14/05/1982"},
+    {id:3, name:"Christine Lobowski", age:"42", col:"green", dob:"22/05/1982"},
+    {id:4, name:"Brendon Philips", age:"125", col:"orange", dob:"01/08/1980"},
+    {id:5, name:"Margret Marmajuke", age:"16", col:"yellow", dob:"31/01/1999"},
+  ];
+  
+  
+  //create Tabulator on DOM element with id "example-table"
+  var t_table = new Tabulator("#tabulator-example-table", {
+    height:205, // set height of table (in CSS or here), this enables the Virtual DOM and improves render speed dramatically (can be any valid css height value)
+    data:tabultorTestData, //assign data to table
+    layout:"fitColumns", //fit columns to width of table (optional)
+    columns:[ //Define Table Columns
+      {title:"Name", field:"name", width:150},
+      {title:"Age", field:"age", hozAlign:"left", formatter:"progress"},
+      {title:"Favourite Color", field:"col"},
+      {title:"Date Of Birth", field:"dob", sorter:"date", hozAlign:"center"},
+    ],
+  
+  });
+  
+  //trigger an alert message when the row is clicked
+  t_table.on("rowClick", function(e, row){ 
+   alert("Row " + row.getData().id + " Clicked!!!!");
+  });
+  
+// Testing Tabulator
+
+
   GbGradeTableEditor.prototype.beginEditing = function() {
     Handsontable.editors.TextEditor.prototype.beginEditing.apply(this, arguments);
 
